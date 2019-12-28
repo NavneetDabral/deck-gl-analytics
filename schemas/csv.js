a0661097d0ba443cf780c981f07f2bb4baf6cb09
Navneet Dabral <dabralnavneet@gmail.com>
2019-12-28 14:20:39 +0530

    Add files via upload

--- schemas/csv.js
+++ schemas/csv.js
@@ -1,1 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var csvSchema = new Schema({
+    id :String,
+    user_id :String,
+    vehical_model_id :String,
+    package_id :String,
+    travel_type :String,
+    from_area_id :String,
+    to_area_id :String,
+    from_city_id :String,
+    to_city_id :String,
+    from_data :String,
+    to_data :String,
+    online_booking :String,
+    mobile_booking :String,
+    booking_created :String,
+    from_lat :String,
+    from_long :String,
+    to_lat :String,
+    to_long :String,
+    car_cancel :String
+})
 
+module.exports = mongoose.model('ride', csvSchema);
